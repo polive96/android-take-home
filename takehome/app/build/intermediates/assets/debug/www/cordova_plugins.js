bf1ab53aa682f0b26624bdb5db81225c7d207c5e
phillip olive <phillip.a.olive@gmail.com>
2018-11-16 22:10:24 -0600

    removing temp files in project

--- takehome/app/build/intermediates/assets/debug/www/cordova_plugins.js
+++
@@ -1,9 +0,0 @@
-cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
-module.exports.metadata = 
-// TOP OF METADATA
-{
-  "com.fivestars.communication": "1.0.0"
-};
-// BOTTOM OF METADATA
-});
